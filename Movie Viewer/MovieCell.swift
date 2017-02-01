45629d8499591d8c73397f2ce4bd36c3216d1e92
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-01-31 23:45:06 -0500

    Adding code for the movie viewer app.

---
+++ Movie Viewer/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Movie Viewer
+//
+//  Created by Sudheesh Bhattarai on 1/31/17.
+//  Copyright © 2017 Sudheesh Bhattarai. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+platform :ios, "8.0"
+inhibit_all_warnings!
+
+target 'Movie Viewer' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+    pod 'AFNetworking'
+    pod 'MBProgressHUD'
+  # Pods for Movie Viewer
+
+end
